52dafb38930a6a6f97509978ac2a1b3e2761b531
Jan Medek <czmedexs@gmail.com>
2025-09-16 16:36:22 +0100

    Removed test-build with mike 2.1.3

--- test-build/doxygen/build/html/search/typedefs_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['unix_5ftcp_5fbuffer_5ft_0',['unix_tcp_buffer_t',['../libtropic__port__unix__tcp_8h.html#a7c5d1d61d3e86200332ecc25e2576c3c',1,'libtropic_port_unix_tcp.h']]],
-  ['unix_5ftcp_5ftag_5ft_1',['unix_tcp_tag_t',['../libtropic__port__unix__tcp_8h.html#a81b4c219d4eebf707c53ac1b03afcd05',1,'libtropic_port_unix_tcp.h']]]
-];
