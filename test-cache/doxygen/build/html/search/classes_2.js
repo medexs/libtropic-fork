3326ef4c6023d615c589a264bde4398d17550222
Jan Medek <czmedexs@gmail.com>
2025-09-19 09:34:58 +0100

    Removed test-cache with mike 2.1.3

--- test-cache/doxygen/build/html/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['session_5fstate_5ft_0',['session_state_t',['../libtropic__common_8h.html#structsession__state__t',1,'']]]
-];
--- test-cache/doxygen/build/html/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['bits_2eh_0',['bits.h',['../bits_8h.html',1,'']]]
-];
